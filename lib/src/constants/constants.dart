57d18123de32892aec1a806cf75502e7f152d777
faqcodes <faqcodes@gmail.com>
2023-07-03 16:50:29 -0400

    fix: add ignore constant_identifier_names

--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 const String TRANSBANK_WEBVIEW_HOST = 'webview.TRANSBANK.com';
 const String TRANSBANK_WEBVIEW_PATH = 'widget.html';
 const String TRANSBANK_WEBVIEW_URL =
